0b08cb632ba61bc2682e621db8fdb28a193dc9cf
zzinue <zzinue@gmail.com>
2022-10-20 18:18:19 -0500

    settings added

--- src/components/pages/settings/Settings.jsx
+++ src/components/pages/settings/Settings.jsx
@@ -17,8 +17,22 @@ export const Settings = () => {
                     <label>
                         Profile Picture
                     </label>
-                    <div className="settingsPP"> +
-                        img</div>
+                    <div className="settingsPP">
+                        <img src="https://assets.justinmind.com/wp-content/webp-express/webp-images/uploads/2019/10/best-20-web-development-blogs.png.webp" alt="" />
+                        <div className='settingsPPIcon'>
+                            <label htmlFor="fileInput">
+                                <i class="fa-regular fa-user"></i>
+                            </label>
+                        </div>
+                        <input type="file" id='fileInput' style={{ display: 'none' }} />
+                    </div>
+                    <label>Username</label>
+                    <input type="text" placeholder='zzinue' />
+                    <label>Email</label>
+                    <input type="text" placeholder='zzinue@gmail.com' />
+                    <label>Password</label>
+                    <input type="password" />
+                    <button className="settingsSubmit">Update</button>
                 </form>
             </div>
             <Sidebar />
